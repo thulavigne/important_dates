a699d2ffb0a1339a9732a550ec591eff4f7754c1
Thu Quach <thuquach90@gmail.com>
2013-07-17 09:41:20 -0700

    Generate app. Add feature so that index only shows upcoming events.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImportantDates::Application.config.secret_token = '751847a3794b7259da2531bcba6dd251620efbf6c43f2260fa6f2fa5e924b40a1dde8abae915dce606f28e4a42f8e42304b053ebdb4010a50bf740d6f432970d'
